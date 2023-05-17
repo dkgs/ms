1f41941a09253a58ad7d323ab95db2b112b1d516
dkgs <mickael.guerra@outlook.com>
2023-05-17 22:28:01 +0200

    main of the server

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include "api/request_dispatcher.hpp"
 #include "api/connection_manager.hpp"
+#include "api/handler/book_seat_handler.hpp"
+#include "api/handler/choose_seat_handler.hpp"
+#include "api/handler/choose_theater_handler.hpp"
 #include "api/handler/root_handler.hpp"
+#include "api/serializer/html_serializer.hpp"
 
 #include "service/booking_service_impl.hpp"
 #include "service/theater_manager_impl.hpp"
@@ -9,6 +13,12 @@
 #include <iostream>
 #include <memory>
 
+/**
+ * @brief Create a and populate booking service object.
+ * Sets all the data in memory.
+ * 
+ * @return std::shared_ptr<service::booking_service_impl> 
+ */
 static
 std::shared_ptr<service::booking_service_impl>
 create_and_populate_booking_service()
@@ -61,10 +71,24 @@ int main(int argc, char * argv[])
 
         auto booking_service = create_and_populate_booking_service();
 
-        using request_dispatcher_t = api::request_dispatcher<api::handler::root_handler>;
+        using request_dispatcher_t = api::request_dispatcher<
+            api::serializer::html_serializer,
+            api::handler::book_seat_handler,
+            api::handler::choose_seat_handler,
+            api::handler::choose_theater_handler,
+            api::handler::root_handler
+        >;
         using connection_manager_t = api::connection_manager<request_dispatcher_t>;
 
-        request_dispatcher_t rd {{api::handler::root_handler{booking_service}}};
+        request_dispatcher_t rd {
+            api::serializer::html_serializer{},
+            {
+                api::handler::book_seat_handler{booking_service},
+                api::handler::choose_seat_handler{booking_service},
+                api::handler::choose_theater_handler{booking_service},
+                api::handler::root_handler{booking_service}
+            }
+        };
         connection_manager_t cm {std::move(rd), booking_service};
         cm.run(argv[1], static_cast<unsigned short>(std::atoi(argv[2])));
 
